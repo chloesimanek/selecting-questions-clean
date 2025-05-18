e915e051038ebacaa1474d587a56dab3a3dbc271
chloesimanek <simanekc@carleton.edu>
2025-05-18 18:46:37 -0500

    test set up

--- make_chart.py
+++ make_chart.py
@@ -1,16 +1,25 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 df = pd.read_csv('results-0.75.csv')
 df.columns = df.columns.str.strip()
 
-plt.figure(figsize=(8, 5))
+plt.figure(figsize=(8, 7))
+custom_colors = ['#1f77b4', '#ff7f0e', '#d62728','#2ca02c']
+plt.yticks(np.arange(0.700, 0.800 + 0.001, 0.005))
 
 # Plot each model as a separate line
-for model in df['model'].unique():
+for i, model in enumerate(df['model'].unique()):
     subset = df[df['model'] == model]
-    plt.plot(subset['n_query'], subset['test_accuracy'], marker='o', label=model)
+    plt.plot(
+        subset['n_query'],
+        subset['test_accuracy'],
+        marker='.',
+        label=model,
+        linewidth=1,
+        color=custom_colors[i % len(custom_colors)])
 
 theoretical_best = 0.757588
 plt.axhline(y=theoretical_best, color='black', linestyle='--')
@@ -20,7 +29,7 @@ plt.ylabel('Test Accuracy')
 plt.title('')
 plt.legend(title='Model')
 plt.grid(False)
-plt.xticks([1, 3, 5, 10])  # Only show specific x-axis values
+plt.xticks([1, 5, 10])  # Only show specific x-axis values
 plt.tight_layout()
 
 
